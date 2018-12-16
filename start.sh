fe77e1e7d79e97fc71649517426d6e83dd6b7482
Kitamura Satoshi <satooshi.jp@gmail.com>
2018-12-16 21:09:23 +0900

    remove db creation tasks

--- start.sh
+++ start.sh
@@ -2,6 +2,4 @@
 
 # rsync
 test "$RAILS_ENV" == "DEVELOPMENT" && rsync -a /bundle/ /app/vendor/bundle/
-bin/rails --trace db:create db:migrate
-bin/rails --trace db:create db:migrate RAILS_ENV=test
 bin/rails s -b 0.0.0.0
