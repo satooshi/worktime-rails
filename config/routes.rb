c8edbd9708deebe9810d4d180143794a3802a8d5
Kitamura Satoshi <satooshi.jp@gmail.com>
2019-07-28 09:08:04 +0000

    bin/rails g active_admin:install AdminUser

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
